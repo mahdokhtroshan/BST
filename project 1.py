c9b04c445a927e244777ecb443038b91d7ee4431
mahdokhtroshan <167623558+mahdokhtroshan@users.noreply.github.com>
2024-06-08 03:23:28 +0330

    Update project 1.py

--- project 1.py
+++ project 1.py
@@ -36,35 +36,15 @@ class Node:
             return self.right.Maximum()
         else:
             return self.data
+        
     def FindNext(self):
-        if self.right:
-            return self.right.Minimum()
+         if self.right:
+             return self.right.Minimum()
     def FindPre(self):
-        if self.left:
-            return self.left.Maximum()
+         if self.left:
+             return self.left.Maximum() 
         
-        
-    def Delete(self , data):
-        if data < self.data:
-            if self.left:
-                self.left = self.left.Delete(data)
-        elif data > self.data:
-            if self.right:
-                self.right = self.right.Delete(data)
-        else:
-            if self.left and self.right:
-                mini = self.right.Minimum()
-                self.data = mini
-                self.right = self.right.Delete(mini)
-            elif self.left:
-                return self.left
-            elif self.right:
-                return self.right
-            else:
-                return None
-            return self
-        
-    
+  
     
 
             
@@ -85,15 +65,71 @@ class BinarySearchTree:
         else:
             print("Tree is empty")
         
-    def Delete(self ,value):
+    def delete(self, data):
         if self.root:
-            self.root = self.root.Delete(value)
-            
-    def FindNext(self):
-        pass
-    def FindPre(self):
-        pass
+            self.root = self.Delete(self.root, data)
+
+    def Delete(self, root, data):
+        if root is None:
+            return root
+        if data < root.data:
+            root.left = self.Delete(root.left, data)
+        elif data > root.data:
+            root.right = self.Delete(root.right, data)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            x = root.right
+            while x.left:
+                x = x.left
+            root.data = x.data
+            root.right = self.Delete(root.right, root.data)
+        return root
+
+  
+          
     
+    def FindNext(self , data):
+        current = self.root
+        successor = None
+        while current:
+            if data < current.data:
+                successor = current
+                current = current.left
+            else:
+                current = current.right
+        return successor.data if successor else None
+    
+    def FindPre(self , data):
+        current = self.root
+        predecessor = None
+        while current:
+            if data > current.data:
+                predecessor = current
+                current = current.right
+            else:
+                current = current.left
+        return predecessor.data if predecessor else None
+        
+    def merge(self, otherbst):
+        elements = []
+        self.InorderElements(self.root, elements)
+        self.InorderElements(otherbst.root, elements)
+        elements.sort()
+        merged_bst = BinarySearchTree()
+        for element in elements:
+            merged_bst.insert(element)
+        return merged_bst
+    
+    def InorderElements(self, node, elements):
+        if node is None:
+            return
+        self._inorder_elements(node.left, elements)
+        elements.append(node.data)
+        self.InorderElements(node.right, elements)
+        
     def k_thelargest(self, k):
       result = []
       self.Reverse(self.root, result, k)
@@ -149,18 +185,12 @@ class MaxHeap:
             self.arr[i], self.arr[largest] = self.arr[largest], self.arr[i]
             self.MaxHeapify(largest)
 
-    
-bst = BinarySearchTree()
-bst.insert(10)
-bst.insert(12)
-bst.insert(8)
-bst.insert(6)
-
-
+             
+        
 
 #k = 2
 #thelargest = bst.k_thelargest(k)
-#print("{k The largest is :}")
+#print("The largest is :" , k)
 
 #heap = MaxHeap(k)
 #for n in thelargest:
@@ -168,52 +198,66 @@ bst.insert(6)
     
 #print("MaxHeap array is :" , heap.arr[:heap.heapsize])
 
-
-     
-
+print("1.insert a node")
+print("2.find the next node")
+print("3.find the previous node")
+print("4.delete a node")
+print("5.merge 2 bst's")
+print("6.put max in maxheap")
+    
+bst = BinarySearchTree()
 while True:
     
-    
-    print("1.insert a node")
-    print("2.find the next node")
-    print("3.find the previous node")
-    print("4.delete a node")
-    print("5.merge 2 bst's")
-    print("6.put max in maxheap")
-    bst = BinarySearchTree()
-
-
-    
-    choice = int(input("what would you like to do?"))
-    
-    #if choice == 0:
-        #bst.inorder_treewalk()
+    choice = int(input("What would you like to do? "))
+    if choice == 1:
+        data = int(input("Enter a node to add: "))
+        bst.insert(data)
+    elif choice == 2:
+        data = int(input("Enter a node to find the next: "))
+        next_node = bst.find_next(data)
+        if next_node is not None:
+            print(f"The next node is: {next_node}")
+        else:
+            print("No next node found")
+    elif choice == 3:
+        data = int(input("Enter a node to find the previous: "))
+        prev_node = bst.find_previous(data)
+        if prev_node is not None:
+            print(f"The previous node is: {prev_node}")
+        else:
+            print("No previous node found")
+    elif choice == 4:
+        data = int(input("Enter a node to delete: "))
+        bst.delete(data)
+        print("The node has been deleted")
         
-    #elif choice == 1:
-           #data = input("Enter a node to Add :")
-           #bst.insert(data)
-              
-    #elif choice == 2:
-        #data = input("Enter a node to find the next :")
-        #bst.find_next(data)
-        
-    #elif choice == 3:
-        #data = input("Enter a node to find the previous :")
-        #bst.find_pre(data)
-        
-    #elif choice == 4:
-        #data = input("Enter a node to delete :")
-        #bst.Delete(data)
-        
-    #elif choice == 5:
+    elif choice == 5:
+        other_bst = BinarySearchTree()
+        n = int(input("Enter the number of nodes for the second BST: "))
+        for _ in range(n):
+            data = int(input("Enter a node to add to the second BST: "))
+            other_bst.insert(data)
+        merged_bst = bst.merge(other_bst)
+        print("Merged BST ): ", end='')
+        merged_bst.inorder_treewalk()
+        print()
          
-    #elif choice == 6:
-        #k = input("Enter a number to create a MaxHeap :")
-        #bst.k_thelargest(k)
-        #heap.InsertMaxHeap(k)
-        
-    #else:
-        #print("Error")
+    elif choice == 6:
+        k = int(input("Enter the number of largest elements to create a MaxHeap: "))
+        the_largest = bst.k_thelargest(k)
+        print(f"{k} the largest elements are: {the_largest}")
+        heap = MaxHeap(k)
+        for n in the_largest:
+            heap.InsertMaxHeap(n)
+        print("Your MaxHeap Array is: ", heap.arr[:heap.heapsize])
+    elif choice == 7:
+        print("In-order traversal: ", end='')
+        bst.inorder_treewalk()
+        print()
+    elif choice == 8:
+        break
+    else:
+        print("Invalid Request")
            
     
         
@@ -230,3 +274,4 @@ while True:
 
     
     
+
